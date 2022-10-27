8c036eb497b29f8411b5c4459c9bba8ae06be674
GoltvyDen <Den.goltvyanskiy206@gmail.com>
2022-10-27 23:54:32 +0300

    Reduced boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './app.scss';
-import Form from './Form';
+import F from './Form';
 
 function App() {
   return (
     <div className="app">
-        <Form/>
+        <F/>
     </div>
   );
 }
